4718d60ad771e20fff7697670d5092a2d26700de
didgustm <bw03306@gmail.com>
2024-11-23 20:05:20 +0900

    1.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,31 @@ const main = document.querySelector('main'),
 sections = document.querySelectorAll('.aos');
 
 // Scroll
-ScrollTrigger.create({
-    trigger: main,
-    onUpdate: target => {
-        sections.forEach((x) => throttle(scrollActive(target, x), 120));
-    }
-});
+sections.forEach((section, index) => {
+    gsap.to(section, {
+        scrollTrigger: {
+            trigger: section,
+            onEnter: () => {
+                section.classList.add("active");
+            },
+            onUpdate: ({progress}) => {
+                if(index === 1){
+                    const scale = progress + 0.7 >= 1? 1: progress + 0.7;
+                    gsap.to(".sct01 .img", {scale})
+                    gsap.to(".sct01 .inner", {scale})
+                }
+            }
+        }
+    })
+})
+
+
+// ScrollTrigger.create({
+//     trigger: main,
+//     onUpdate: target => {
+//         sections.forEach((x) => throttle(scrollActive(target, x), 120));
+//     }
+// });
 
 // Scroll Active
 function scrollActive(target, x){
